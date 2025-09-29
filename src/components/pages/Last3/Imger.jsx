5e16f78d21e018c07bd4ff05dbe1c98594ba4821
arunchakrawarti <arunmojampur@gmail.com>
2025-09-29 15:51:20 +0530

    upload

--- src/components/pages/Last3/Imger.jsx
+++ src/components/pages/Last3/Imger.jsx
@@ -1,49 +1,31 @@
-
 import LoveCard from '@/components/molecules/LoveCard'
 import React from 'react'
 
-
 const Imger = () => {
     let arr = [
-        {
-            img:"/img/story1.png"
-        },
-        {
-            img:"/img/story1.png"
-        },
-        {
-            img:"/img/story1.png"
-        },
-        {
-            img:"/img/story1.png"
-        },
-        {
-            img:"/img/story1.png"
-        },
-        {
-            img:"/img/story1.png"
-        },
-       
-    ]
-  return (
-    <div className='sm:mx-17 mt-10'>
-    <div className='flex mb-2 mr-15 justify-between'>
-    <h1 className='text-3xl font-bold'>Chapter</h1>
-    <p>Our Dekhe</p>
-    
-    </div>
-     <div className="flex gap-10 mt-5">
-            {
-                arr.map((item, i) => {
-                    return (<LoveCard 
-                        key={i}     
-                        img={item?.img} 
-                    />)
-                })
-            }
+        { img: "/img/story1.png" },
+        { img: "/img/story1.png" },
+        { img: "/img/story1.png" },
+        { img: "/img/story1.png" },
+        { img: "/img/story1.png" },
+        { img: "/img/story1.png" },
+    ];
+
+    return (
+        <div className='sm:mx-17 mt-10'>
+            <div className='flex mb-2 justify-between items-center'>
+                <h1 className='text-3xl font-bold'>Chapter</h1>
+                <p>Our Dekhe</p>
             </div>
-    </div>
-  )
+
+           
+            <div className="grid grid-cols-2 gap-4 mt-5 sm:flex sm:gap-10">
+                {arr.map((item, i) => (
+                    <LoveCard key={i} img={item.img} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Imger
+export default Imger;
--- src/components/pages/Last3/Photo21.jsx
+++ src/components/pages/Last3/Photo21.jsx
@@ -3,22 +3,26 @@ import React from 'react'
 
 const Photo21 = () => {
   return (
-    <div className='flex w-[90%] mt-10 m-auto justify-between'>
-      <div className='w-[20%]'>
+    <div className='flex flex-col sm:flex-row w-[90%] mt-10 mx-auto justify-between gap-6'>
+      
+      {/* Left section */}
+      <div className='w-full sm:w-[20%] flex flex-col items-center sm:items-start'>
         <Image
-        src="/img/man.png"
-        height={160}
-        width={160}
-        alt='man.png'
+          src="/img/man.png"
+          height={160}
+          width={160}
+          alt='man.png'
         />
-        
-        <p className='text-xl font-bold mt-1'>:);)sarkar...."तुतुन"</p>
-        <h2 className='mt-1 ml-5'>7130 लोगों ने पढ़ा।</h2>
-        
+        <p className='text-xl font-bold mt-2 text-center sm:text-left'>:);)sarkar...."तुतुन"</p>
+        <h2 className='mt-1 text-center sm:text-left'>7130 लोगों ने पढ़ा।</h2>
       </div>
-      <div className='w-[50%] mt-10'>
+
+      {/* Right section */}
+      <div className='w-full sm:w-[50%] mt-4 sm:mt-10'>
         <h1 className='mt-2 font-bold text-lg'>लेखक के बारे में</h1>
-        <p className='mt-2'>रमेश कुमार एक प्रसिद्ध हिंदी लेखक हैं। उन्होंने अपनी पढ़ाई दिल्ली विश्वविद्यालय से पूरी की। रमेश जी ने कहानी, उपन्यास और कविता में महत्वपूर्ण योगदान कविता में महत्वपूर्ण योगदान  दिया है।  ”</p>
+        <p className='mt-2'>
+          रमेश कुमार एक प्रसिद्ध हिंदी लेखक हैं। उन्होंने अपनी पढ़ाई दिल्ली विश्वविद्यालय से पूरी की। रमेश जी ने कहानी, उपन्यास और कविता में महत्वपूर्ण योगदान कविता में महत्वपूर्ण योगदान दिया है।
+        </p>
       </div>
     </div>
   )
