5eb8ba7bfe114deff13c20aa21613ab3eaf451c1
arunchakrawarti <arunmojampur@gmail.com>
2025-09-29 15:42:30 +0530

    loded

--- src/components/pages/Last3/Imger.jsx
+++ src/components/pages/Last3/Imger.jsx
@@ -23,18 +23,16 @@ const Imger = () => {
         {
             img:"/img/story1.png"
         },
-        {
-            img:"/img/story1.png"
-        },
+       
     ]
   return (
-    <div className='ml-17 mt-10'>
+    <div className='sm:mx-17 mt-10'>
     <div className='flex mb-2 mr-15 justify-between'>
     <h1 className='text-3xl font-bold'>Chapter</h1>
     <p>Our Dekhe</p>
     
     </div>
-     <div className="flex gap-4 mt-5">
+     <div className="flex gap-10 mt-5">
             {
                 arr.map((item, i) => {
                     return (<LoveCard 
