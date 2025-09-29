aa0988aecf36444978ff73b911fc3314e83c8b4c
arunchakrawarti <arunmojampur@gmail.com>
2025-09-29 14:50:50 +0530

    sumit

--- src/components/pages/Designnn/Man.jsx
+++ src/components/pages/Designnn/Man.jsx
@@ -1,42 +1,5 @@
-// import CollectionCard from '@/components/molecules/CollectionCard'
-// import React from 'react'
-
-// const Man = () => {
-//     let arr = [
-//         {
-//             image:"/img/Collection.png",
-//         },
-//         {
-//             image:"/img/Collection.png",
-//         },
-//         {
-//             image:"/img/Collection.png",
-//         },
-       
-//     ]
-//   return (
-//    <div className="w-full mx-auto mt-15">
-//       <h1 className="text-4xl font-bold mb-10">Collection of Hindi Stories</h1>
-      
-//       <div className="grid grid-row-3 gap-10">
-//         {/* col-1 → 3 images */}
-//         <div className="flex flex-row gap-15">
-//           {arr.slice(0, 3).map((item, i) => (
-//             <CollectionCard key={i} img={item.image} />
-//           ))}
-//         </div>
-
-        
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Man
-
-
-import CollectionCard from '@/components/molecules/CollectionCard'
-import React from 'react'
+import CollectionCard from '@/components/molecules/CollectionCard';
+import React from 'react';
 
 const Man = () => {
   let arr = [
@@ -46,17 +9,19 @@ const Man = () => {
   ];
 
   return (
-    <div className="w-[90%] mx-auto mt-15">
-      <h1 className="text-4xl font-bold mb-10">Collection of Hindi Stories</h1>
+    <div className="w-[95%] sm:w-[100%] mx-auto mt-15">
+      <h1 className="text-4xl font-bold mb-10 ml-5 text-center sm:text-left">Collection of Hindi Stories</h1>
 
-      {/* Responsive Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+      {/* Responsive Grid: 1 col mobile, 2 col tablet, 3 col large */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
         {arr.map((item, i) => (
-          <CollectionCard key={i} img={item.image} />
+          <div key={i} className="w-full sm:w-[90%] lg:w-auto">
+            <CollectionCard img={item.image} />
+          </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default Man;
